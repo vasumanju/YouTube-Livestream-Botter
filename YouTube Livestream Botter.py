5199976623010fdece425b95c262618eb82d6407
vasumanju <76543766+vasumanju@users.noreply.github.com>
2020-12-24 08:23:50 +0530

    D5E4ZZI7t64
    D5E4ZZI7t64

--- YouTube Livestream Botter.py
+++ YouTube Livestream Botter.py
@@ -114,7 +114,7 @@ def bot():
             of = url.split("of=")[1].split("&")[0]
             vm = url.split("vm=")[1].split("&")[0]
             s.get("https://s.youtube.com/api/stats/watchtime?ns=yt&el=detailpage&cpn=isWmmj2C9Y2vULKF&docid=" + token + "&ver=2&cmt=7334&ei=" + ei + "&fmt=133&fs=0&rt=1003&of=" + of +"&euri&lact=4418&live=dvr&cl=" + cl + "&state=playing&vm=" + vm + "&volume=100&c=MWEB&cver=2.20200313.03.00&cplayer=UNIPLAYER&cbrand=apple&cbr=Safari%20Mobile&cbrver=12.1.15E148&cmodel=iphone&cos=iPhone&cosver=12_2&cplatform=MOBILE&delay=5&hl=ru&cr=GB&rtn=1303&afmt=140&lio=1556394045.182&idpj=&ldpj=&rti=1003&muted=0&st=7334&et=7634",headers={'Host': 's.youtube.com', 'Proxy-Connection': 'Keep-Alive', 'User-Agent': ua, 'Accept': 'image/png,image/svg+xml,image/*;q=0.8,video/*;q=0.8,*/*;q=0.5', 'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3', 'Referer': 'https://m.youtube.com/watch?v=' + token},proxies=random_proxy)   # API GET request
-
+D5E4ZZI7t64
             a.botted += 1
         except:
             pass
@@ -126,8 +126,8 @@ maxthreads = int(input("How many Threads? Recommended: 500 - 1000\n"))
 threading.Thread(target=a.printservice).start()
 num = 0
 while num < maxthreads :
-    num += 1
-    threading.Thread(target=bot).start()
+    num += 1000
+    threading.Thread(target=bot).start()https://D5E4ZZI7t64
 
 
 threading.Thread(target=bot).start()
